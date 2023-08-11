1598f6980f74f82d6d112913841db3df422c31d8
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-11 20:21:49 -0300

    Add files via upload

---
+++ TP4-10.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int i;
+	char a[4] = "Hola";
+	char b[4];
+	int variable = 0;
+	
+	for(i=0; a[i]!=0; i++){
+		printf("%c", a[i]);
+	}
+	
+	for (i=4; i>0; i--){
+		b[variable]= a[i];
+		variable++;
+	}
+	
+	for(i=0; a[i]!=0; i++){
+		printf("%c", b[i]);
+	}
+	
+	return 0;
+}
